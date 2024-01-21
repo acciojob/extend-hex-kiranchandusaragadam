1362b274a521abbcb25665159e799571444ca169
Kiranchandu Saragadam <110031953+kiranchandusaragadam@users.noreply.github.com>
2024-01-21 21:12:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let hex = "#";
+	if(shortHex.charAt(0) !== '#'){
+		shortHex = '#' + shortHex;
+	}
+	
+	let code = shortHex.charCodeAt(1);
+	if(code >= 65 && code <= 90){
+		shortHex = shortHex.toUpperCase();
+	}
+	else if(code >= 97 && code <= 122){
+		shortHex = shortHex.toLowerCase();
+	}
+	
+	for(let i=1; i<shortHex.length; i++){
+		let ch = shortHex.charAt(i);
+		hex += ch + ch;
+	}
+
+	return hex;
 };
 
 // Do not change the code below.
